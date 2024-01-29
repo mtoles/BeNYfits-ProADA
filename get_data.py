54a1a70cc2c2dcf5cfdfe45fccb9cbd00ba20581
destigres <toles.matthew@gmail.com>
2024-01-29 10:39:37 -0500

    feat: script for checking primary model vibes

--- get_data.py
+++ get_data.py
@@ -2,7 +2,117 @@ from google.cloud import bigquery
 import pandas as pd
 from tqdm import tqdm
 import click
+import json
+import re
 
+tldr_versions = [
+    "tl dr",
+    "tl;dr",
+    "tldr",
+    "tl:dr",
+    "tl/dr",
+    "tl; dr",
+    "tl,dr",
+    "tl, dr",
+    "tl-dr",
+    "tl’dr",
+    "tl: dr",
+    "tl.dr",
+    "tl ; dr",
+    "tl dr",
+    "tldr;dr",
+    "tl ;dr",
+    "tl\\\\dr",  # backslash hell
+    "tl/ dr",
+    "tld:dr",
+    "tl;;dr",
+    "tltl;dr",
+    "tl˜dr",
+    "tl / dr",
+    "tl :dr",
+    "tl - dr",
+    "tl\\\\\\\\dr",  # double backslash hell
+    "tl. dr",
+    "tl:;dr",
+    "tl|dr",
+    "tl;sdr",
+    "tll;dr",
+    "tl : dr",
+    "tld;dr"
+]
+
+
+def filter_df(df: pd.DataFrame) -> bool:
+    filtered_rows = []
+
+    result = []
+
+    def additional_constraint_and_parsing(content, summary):
+        # Summary does not starts with tldr (Stripped off before calling this function)
+        # Remove these words from summary if it starts with :;,.
+        summary = summary.lstrip(":;,.")
+        # summary = summary[1:] if summary.startswith(":") else summary
+
+        # If post is of the format - [Content-Part1(tldr)Summary \n\n Content-Part2] i.e. contains new line in summary
+        # Then we are concatenating content part1 and part2 to form complete content and portion 
+        newlines_index_in_summary = summary.find('\n\n')
+
+        if newlines_index_in_summary != -1:
+            content_second_part = summary[newlines_index_in_summary:]
+            summary = summary[:newlines_index_in_summary]
+            content = content + content_second_part        
+
+        # The summary length is less than 10% of length of total post then only consider the same
+        if len(summary) > 0.10 * len(selftext):
+            return None
+        
+        return result
+
+    # with open(jsonl_file_path, 'r') as file:
+    for index, row in tqdm(df.iterrows()):
+        # for line in file:    
+        for line in row['selftext'].splitlines():
+            # data = json.loads(line)
+            selftext = line.get('selftext', '')
+            subreddit = line.get('subreddit', '')
+            score = line.get('score', '')
+
+            # Replace more than two contiguous '\n' with two '\n\n'
+            selftext = re.sub(r'\n{3,}', r'\n\n', selftext)
+
+            tldr_indexes = []
+            final_tldr_versions = []
+
+            for version in tldr_versions:
+                if version in selftext.lower():                
+                    cur_tldr_index = selftext.lower().find(version)
+
+                    # Continue searching in selftext until no more occurrences of current version are found
+                    while cur_tldr_index != -1:  
+                        tldr_indexes.append(cur_tldr_index)
+                        final_tldr_versions.append(version)
+                        cur_tldr_index = selftext.lower().find(version, cur_tldr_index + 1)
+        
+            if len(tldr_indexes) == 1:
+                # Condition when there is only one instance of tldr present in the text
+                # Rest all cases are filtered out
+                tldr_index = tldr_indexes[0]
+                final_tldr_version = final_tldr_versions[0]
+                
+                if tldr_index == 0:
+                    # selftext starting with tldr. split by first `\n\n` - [(tldr)Summary \n\n Content]
+                    newlines_index = selftext.find('\n\n')
+
+                    if newlines_index != -1:
+                        content = selftext[newlines_index + len('\n\n'):]
+                        summary = selftext[len(final_tldr_version):newlines_index]
+                        return additional_constraint_and_parsing(content, summary)
+
+                else:
+                    content = selftext[:tldr_index]
+                    summary = selftext[tldr_index + len(final_tldr_version):]
+
+                    return additional_constraint_and_parsing(content, summary)    
 
 @click.command()
 @click.option(
@@ -72,41 +182,6 @@ def main(tldr: str):
     df_list = []
 
     for table in tqdm(tables):
-        tldr_versions = [
-            "tl dr",
-            "tl;dr",
-            "tldr",
-            "tl:dr",
-            "tl/dr",
-            "tl; dr",
-            "tl,dr",
-            "tl, dr",
-            "tl-dr",
-            "tl’dr",
-            "tl: dr",
-            "tl.dr",
-            "tl ; dr",
-            "tl dr",
-            "tldr;dr",
-            "tl ;dr",
-            "tl\\\\dr",  # backslash hell
-            "tl/ dr",
-            "tld:dr",
-            "tl;;dr",
-            "tltl;dr",
-            "tl˜dr",
-            "tl / dr",
-            "tl :dr",
-            "tl - dr",
-            "tl\\\\\\\\dr",  # double backslash hell
-            "tl. dr",
-            "tl:;dr",
-            "tl|dr",
-            "tl;sdr",
-            "tll;dr",
-            "tl : dr",
-            "tld;dr",
-        ]
         tldr_block = (
             "AND (\n"
             + " OR \n".join(
@@ -140,6 +215,10 @@ def main(tldr: str):
 
     # concatenate all dataframes
     df = pd.concat(df_list)
+
+    # apply the tldr filter if specified
+    if tldr:
+        df = filter_df(df)
     output_path = (
         "full_data/reddit_tldr_dataset.jsonl"
         if tldr
