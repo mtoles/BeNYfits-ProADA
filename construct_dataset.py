5ec460f00f05a3e0bd60c06e7424839f17aef849
Nikhil Balwani <nikhilbalwani1998@gmail.com>
2024-12-02 04:03:04 -0800

    feat: edge case dataset initial commit

---
+++ construct_dataset.py
@@ -0,0 +1,23 @@
+from analysis.dataset_constructor import DatasetConstructor
+from users.benefits_programs import ChildAndDependentCareTaxCredit, EarlyHeadStartPrograms, InfantToddlerPrograms, ComprehensiveAfterSchool, InfantToddlerPrograms, ChildTaxCredit, DisabilityRentIncreaseExemption, EarnedIncomeTaxCredit, HeadStart
+import json
+
+
+households = [hh for hh in DatasetConstructor.fuzz()]
+households_members = [eval(str(hh)) for hh in households]
+
+with open("edge_case_dataset.jsonl", "w") as fout:
+
+    for hh, members in zip(households, households_members):
+        household_dict = {"hh": {"features": {"members": []}}}
+
+        for member in members:
+            household_dict["hh"]["features"]["members"].append({"features": member})
+
+        household_dict["hh_nl_desc"] = hh.nl_household_profile()
+        household_dict["note"] = ""
+
+        for program in [ChildAndDependentCareTaxCredit, EarlyHeadStartPrograms, InfantToddlerPrograms, ComprehensiveAfterSchool, InfantToddlerPrograms, ChildTaxCredit, DisabilityRentIncreaseExemption, EarnedIncomeTaxCredit, HeadStart]:
+            household_dict[program.__name__] = program.__call__(hh)
+
+        fout.write(json.dumps(household_dict) + "\n")
